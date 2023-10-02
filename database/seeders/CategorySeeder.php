7a2a13bfaccafddc82e553e7800257802760dc24
Jesus Chacon <jesus.chacon@novadeha.com>
2023-10-02 16:34:00 -0400

    integrate migration and seed

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('categories')->insert(['name' => 'Electronics']);
+        DB::table('categories')->insert(['name' => 'Home']);
+        DB::table('categories')->insert(['name' => 'Men\'s Fashion']);
+        DB::table('categories')->insert(['name' => 'Women\'s Fashion']);
+        DB::table('categories')->insert(['name' => 'Pets']);
+        DB::table('categories')->insert(['name' => 'Arts & Crafts']);
     }
 }
